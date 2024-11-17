46d2212f88fe334296b027f55c8a1f1a0a346999
Mitchell Krieger <mitkrieger@gmail.com>
2024-11-17 12:12:51 -0500

    task 3.5

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -30,7 +30,9 @@ class Network(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        x = self.layer1(x).relu()
+        x = self.layer2(x).relu()
+        return self.layer3(x).sigmoid()
 
 
 class Linear(minitorch.Module):
@@ -44,7 +46,7 @@ class Linear(minitorch.Module):
 
     def forward(self, x):
         # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        return x @ self.weights + self.bias.value
 
 
 class FastTrain:
